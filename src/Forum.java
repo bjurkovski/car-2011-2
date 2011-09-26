67e8cd97b1079a229458d3b44e750d13332c5a46
Bruno Jurkovski <bjurkovski@gmail.com>
2011-09-26 17:00:13 +0200

    First Commit

---
+++ src/Forum.java
@@ -0,0 +1,28 @@
+import java.rmi.*;
+
+public interface Forum extends Remote {
+	/**
+ * Enregistre un intervanant dans la structure de mémoristion des intervenants. Cette méthode est
+ * appelée par le traitant de communication du programme client (IntervenantImpl) 
+ * @param intervenant une reference distante vers l'intervenant
+ * @param nom nom de l'intervenant
+ * @param prenom prenom de l'intervenant
+ * @return un identifiant interne representant l'intervenant 
+ * dans la structure de mémoristion des intervenants
+ */
+  public  HashMap enter (Intervenant intervenant, String prenom, String nom)throws RemoteException;
+  
+   /**
+ * De-enregistre un intervanant dans la structure de mémoristion des intervenants. Cette méthode est
+ * appelée par le traitant de communication du programme client (IntervenantImpl) 
+ * @param id identification de l'intervenant retourne lors de l'appel à la methode enter.
+ */
+  public  void leave(int id) throws RemoteException;
+  
+ 
+}
+
+
+
+    
+  
